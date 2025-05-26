3e3c7a468cb9ad48e6b2ef5496c25503862e843d
alejandrodomingueztorres <85324176+alejandrodomingueztorres@users.noreply.github.com>
2025-05-25 22:04:07 -0500

    Test3(nivel3)

--- Assets/Scripts/LSUIManager.cs
+++ Assets/Scripts/LSUIManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
 
 /// <summary>
 /// Controlador de la interfaz de usuario para la selección de niveles.
